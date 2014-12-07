83d334c4a014af5e397be7b26efeb66e7da0a15e
zero <zero_2009@hotmail.co.uk>
2014-12-07 01:30:32 +0000

    basic stuff

--- src/basicApplication/gui/GraphicUI.java
+++ src/basicApplication/gui/GraphicUI.java
@@ -5,5 +5,17 @@ import javax.swing.JFrame;
 public class GraphicUI extends JFrame{
 
 	private static final long serialVersionUID = 1L;
+	
+	public GraphicUI()
+	{
+		setLayout();
+	}
 
+	private void setLayout()
+	{
+		setTitle(getName());
+		setSize(1000, 600);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 }
--- src/basicApplication/mainExe/MainClass.java
+++ src/basicApplication/mainExe/MainClass.java
@@ -1,5 +1,11 @@
 package basicApplication.mainExe;
 
+import basicApplication.gui.GraphicUI;
+
 public class MainClass {
 
+	public static void main(String[] args)
+	{
+		GraphicUI graphicUI = new GraphicUI();
+	}
 }
