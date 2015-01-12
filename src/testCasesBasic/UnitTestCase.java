6004e1061cd2eb42e49d00856de59e2fe9c06535
zero <zero_2009@hotmail.co.uk>
2015-01-12 21:59:17 +0000

    deleted stuff

--- src/testCasesBasic/UnitTestCase.java
+++ src/testCasesBasic/UnitTestCase.java
@@ -1,20 +1,9 @@
 package testCasesBasic;
 
-import static org.junit.Assert.*;
-
-import java.util.Iterator;
-import java.util.List;
-
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.junit.Before;
 import org.junit.Test;
 
-import basicApplication.dataClass.ActorHibernateBasic;
 import basicApplication.databaseEngine.HibernateUtilitiess;
 
 public class UnitTestCase {
