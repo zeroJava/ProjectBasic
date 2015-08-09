56fc2b4da9bd689b4f606eb750a19081182a336f
zero <zero_2009@hotmail.co.uk>
2015-08-09 01:54:33 +0100

    Going to make some changes

---
+++ src/basicApplication/gui/ActionClassess.java
@@ -0,0 +1,50 @@
+package basicApplication.gui;
+
+
+public class ActionClassess {
+
+	/*public class RadioButtonActionListener implements ActionListener
+	{
+		private SearchOptionDialogBox dialogbox;
+		
+		public RadioButtonActionListener(SearchOptionDialogBox dialogbox)
+		{
+			this.dialogbox = dialogbox;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+			switch (e.getActionCommand())
+			{
+				case GUIConstance.ALL:
+					dialogbox.searchOption = e.getActionCommand();
+					setOfTextField(stateOfTextField, false, false);
+					minGUI.displayLog("Clicked on radio button and chose  " + GUIConstance.ALL);
+					break;
+					
+				case GUIConstance.FIRST_NAME:
+					searchOption = e.getActionCommand();
+					setOfTextField(!stateOfTextField, true, true);
+					minGUI.displayLog("Clicked on radio button and chose  " + GUIConstance.FIRST_NAME);
+					break;
+					
+				case GUIConstance.LAST_NAME:
+					searchOption = e.getActionCommand();
+					setOfTextField(!stateOfTextField, true, true);
+					minGUI.displayLog("Clicked on radio button and chose  " + GUIConstance.LAST_NAME);
+					break;
+					
+				case GUIConstance.ID:
+					//setSearchOption(e.getActionCommand());
+					searchOption = e.getActionCommand();
+					setOfTextField(!stateOfTextField, true, true);
+					minGUI.displayLog("Clicked on radio button and chose  " + GUIConstance.ID);
+					break;
+					
+				default:
+					break;
+			}
+		}
+	}*/
+}
